859fd9340d7e627a7c7b4ed64a749944e5f85656
lukecharta <Luke@getcharta.com>
2023-02-14 13:10:40 -0500

    Adjusted moment code

--- resources.js
+++ resources.js
@@ -550,8 +550,8 @@ const Resources = () => {
           {allCards
             .sort(
               (a, b) =>
-                new window.moment(a.sortDate).format("YYYYMMDD") -
-                new window.moment(b.sortDate).format("YYYYMMDD")
+                window.moment(a.sortDate).format("YYYYMMDD") -
+                window.moment(b.sortDate).format("YYYYMMDD")
             )
             .map((card) => {
               if (card.contentType == "blog") {
