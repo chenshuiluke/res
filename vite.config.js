a3581fa1263633f3bed04507dafcd047f256531e
lukecharta <Luke@getcharta.com>
2023-06-16 04:01:46 -0500

    Possible fix

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 const path = require("path");
-const { getBabelOutputPlugin } = require("@rollup/plugin-babel");
+import { babel } from "@rollup/plugin-babel";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: "runtime",
+      extensions: [".js", ".jsx", ".es6", ".es", ".mjs", "ts"],
+    }),
+  ],
   build: {
-    rollupOptions: {
-      plugins: [
-        /**
-         * Running Babel on the generated code:
-         *  https://github.com/rollup/plugins/blob/master/packages/babel/README.md#running-babel-on-the-generated-code
-         *
-         * Transforming ES6+ syntax to ES5 is not supported yet, there are two ways to do:
-         *  https://github.com/evanw/esbuild/issues/1010#issuecomment-803865232
-         * We choose to run Babel on the output files after esbuild.
-         *
-         * @vitejs/plugin-legacy does not support library mode:
-         *  https://github.com/vitejs/vite/issues/1639
-         */
-        getBabelOutputPlugin({
-          allowAllFormats: true,
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                useBuiltIns: false, // Default：false
-                // Exclude transforms that make all code slower
-                exclude: ["transform-typeof-symbol"],
-                // https://babeljs.io/docs/en/babel-preset-env#modules
-                modules: false,
-              },
-            ],
-          ],
-          plugins: [
-            /**
-             * Extract the helper function.
-             */
-            [
-              "@babel/plugin-transform-runtime",
-              {
-                corejs: false,
-                // version: require('@babel/runtime').version,
-              },
-            ],
-          ],
-        }),
-      ],
-    },
     lib: {
       entry: path.resolve(__dirname, "resources.jsx"),
       name: "ResourcesLib",
