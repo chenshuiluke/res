b4bff731413442b470dafa7dc8c91a87e1a58e43
lukecharta <Luke@getcharta.com>
2023-08-04 13:38:40 -0500

    Added gold

--- partners.jsx
+++ partners.jsx
@@ -93,7 +93,76 @@ const Partners = ({ scrollPosition }) => {
       </>
     );
   };
-  return renderDiamondPartners(diamondPartners);
+  const renderGoldPartner = (partner) => {
+    return (
+      <>
+        <div
+          id="w-node-_6b17fc24-029e-b47a-7b02-91a17216e4af-d96ba4d7"
+          class="partner-card non-gold-partner"
+        >
+          <div class="div-block-61">
+            <img
+              src={partner.image}
+              loading="lazy"
+              sizes="(max-width: 479px) 91vw, (max-width: 767px) 89vw, (max-width: 991px) 92vw, (max-width: 1279px) 24vw, (max-width: 1919px) 21vw, 293.375px"
+              alt=""
+            />
+            <div class="div-block-60 non-gold">
+              <img
+                src="https://assets-global.website-files.com/6266d8ef8c92b1230d1e0cbb/64c7ec962dec3c5d9ffb1ace_medal-solid.svg"
+                loading="lazy"
+                width="32"
+                height="20"
+                alt=""
+                class="image-32"
+              />
+              <div class="paragraph gold-certified-partner non-gold">
+                {partner.topPerformerCategory}
+              </div>
+            </div>
+          </div>
+          <div class="partner-card-bottom non-gold-partner">
+            <div class="paragraph certified-partner-card-sub">
+              Consulting Partner
+            </div>
+            <div class="paragraph certified-partner-gold-card-heading">
+              {partner.name}
+            </div>
+            <div class="paragraph partner-card-desciption non-gold-description">
+              {partner.description}
+            </div>
+            <div class="card-btn-wrapper certified-partner non-gold-partner">
+              <div class="card-btn line-btn blog-btn partner">Learn More</div>
+              <a href="#" class="link-block-3 non-gold-partner w-inline-block">
+                <img
+                  src={partner.partnerWebsite}
+                  loading="lazy"
+                  width="16"
+                  height="16"
+                  alt=""
+                  class="image-31"
+                />
+                <div class="paragraph">Visit Partner's Website</div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  const renderGoldPartners = (goldPartners) => {
+    <div class="w-layout-grid grid-16">
+      {goldPartners.map((partner) => {
+        return renderGoldPartner(partner);
+      })}
+    </div>;
+  };
+  return (
+    <>
+      {renderDiamondPartners(diamondPartners)}
+      {renderGoldPartners(goldPartners)}
+    </>
+  );
 };
 
 const component = trackWindowScroll(Partners);
