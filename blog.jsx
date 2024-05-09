80769f94236d4774159a423459f137eeef2eed3a
Luke Chen Shui <chenshuiluke@gmail.com>
2024-05-09 09:15:11 -0500

    Fixed undefined error

--- blog.jsx
+++ blog.jsx
@@ -1,5 +1,6 @@
 import StandaloneResource from "./standalone_resource";
 import ReactDOM from "react-dom";
+const e = React.createElement;
 
 const component = <StandaloneResource contentType={"blog"} />;
 const domContainer = document.querySelector("#root");
